ab384d1a8534a433c394fae4d169763762738898
Akan <23879@iitu.kz>
2020-01-27 19:42:27 +0600

    Initial Commit

---
+++ iMusic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iMusic
+//
+//  Created by Akan Akysh on 1/27/20.
+//  Copyright © 2020 Akysh Akan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
